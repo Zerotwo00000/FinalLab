6d13e9ec02c1d817b905e12b84e156344f3f6474
Zerotwo00000 <3293529428@qq.com>
2026-01-03 14:40:19 +0800

    初始化项目代码

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "masterview.h"
+#include "login.h"
+#include <QApplication>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MasterView w;
+    w.show();
+    return a.exec();
+}
